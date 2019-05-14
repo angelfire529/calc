2a193184dcbefceaadd1e99a96dd9a5fdfe23dec
jamie broberg <angelfire529@yahoo.com>
2019-05-13 17:03:23 -0700

    adding slider

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
+import './Calculator.scss';
+import Slider from './Slider';
 
 class Calculator extends Component {
     render() {
         return (
-            <div></div>
+            <div className="row">
+                <div className="col-8 cal-container">
+                <div className="card">
+                <div className="card-body">
+                    <h6 className="card-title">Mortgage Calculator</h6>
+                    <form className="">
+                        <div className="form-group">
+                        <label className="">Years of mortgage</label>
+                        <Slider />
+
+                        </div>
+                    </form>
+                </div>
+                </div>
+                </div>
+                <div className="col-4 results"></div>
+            </div>
         );
     }
 }
+
+
 
 export default Calculator;
---
+++ src/Slider.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react'
+import './Slider.scss';
+
+class Slider extends Component {
+    constructor() {
+        super();
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (e) => {
+        const currentVal = e.target.value;
+        const sheet = document.createElement('style');
+        document.body.appendChild(sheet);
+        const getTrackStyle = (val) => {
+            const prefs = ['webkit-slider-runnable-track', 'moz-range-track', 'ms-track'];
+            let style = '';
+            for(let i =0; i < prefs.length; i++) {
+                style += `.range::-${prefs[i]} {background: linear-gradient(to right, blue 0%, blue ${val}%, #ddd ${val + 1}%, #ddd 100%)}`
+            }
+            return style;
+        };
+        
+        sheet.textContent = getTrackStyle(currentVal);
+        console.log(e)
+    }
+
+    render() {
+        return (
+            <div className="slide-container">
+            <input type="range" className="range" min="1" max="40" default="5" onInput={this.handleChange}></input>
+            </div>
+        );
+    }
+}
+
+
+// var getTrackStyle = function (el) {
+// 	var curVal = el.value,
+// 		style = '';
+
+// 	for (var i = 0; i < prefs.length; i++) {
+// 		style += '.range::-' + prefs[i] + '{background: linear-gradient(to right, #34495e 0%, #34495e ' + curVal + '%, #fff ' + curVal + '%, #fff 100%)}';
+// 	}
+
+// 	return style;
+// }
+
+// $rangeInput.on('input', function () {
+// 	sheet.textContent = getTrackStyle(this);
+// });
+
+export default Slider;
